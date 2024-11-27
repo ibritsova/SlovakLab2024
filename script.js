8c9e76a186109bfccbedb37b19bac0883d62c70e
unknown <ira.britsova18112004@gmail.com>
2024-11-27 18:43:47 +0100

    responsiveInfoAboutCourses

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 new Swiper(".card-wrapper", {
   loop: true,
-  slidesPerView: 2,
+  slidesPerView: 2, // Базова кількість слайдів
   spaceBetween: 20,
-  // pagination bullets
+  centeredSlides: true,
+
+  // Адаптивні налаштування
+  breakpoints: {
+    // Мобільні екрани
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    // Планшети
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      centeredSlides: false,
+    },
+    // Десктопи
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      centeredSlides: false,
+    },
+  },
+
+  // Pagination bullets
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
